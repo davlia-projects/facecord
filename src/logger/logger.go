da323e33bbbfdcc79d3222899bfbf97e93bb1996
davlia <danedaworld@gmail.com>
2018-04-07 22:07:49 -0700

    woops fixing logger

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -24,23 +24,23 @@ func SetLevel(level Level) {
 }
 
 func printf(level Level, tag, msg string, args ...interface{}) {
-	if l.level < level {
+	if l.level <= level {
 		tagFmt := msg
 		if tag != "" {
 			tagFmt = fmt.Sprintf("%s: %s\n", tag, msg)
 		}
-		log.Printf(tagFmt, args)
+		log.Printf(tagFmt, args...)
 	}
 }
 
 func Info(tag, msg string, args ...interface{}) {
-	printf(InfoLevel, tag, msg, args)
+	printf(InfoLevel, tag, msg, args...)
 }
 
 func Debug(tag, msg string, args ...interface{}) {
-	printf(DebugLevel, tag, msg, args)
+	printf(DebugLevel, tag, msg, args...)
 }
 
 func Error(tag, msg string, args ...interface{}) {
-	printf(ErrorLevel, tag, msg, args)
+	printf(ErrorLevel, tag, msg, args...)
 }
